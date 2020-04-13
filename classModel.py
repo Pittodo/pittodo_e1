a8436e0f6675360a203560c1240fc94bde1ff091
pit3r <qpwdreg@zoho.com>
2020-04-13 13:09:58 +0200

    Classes model and task has been added

---
+++ classModel.py
@@ -0,0 +1,28 @@
+
+
+class Model:
+
+    def __init__(self):
+        self.tasks = []
+        pass
+
+    def add_task(task):
+        pass
+
+    def del_task(task_id):
+        pass
+
+    def get_task(task_id):
+        pass
+
+    def save_tasks():
+        pass
+
+    def load_tasks():
+        pass
+
+    def save_settings():
+        pass
+
+    def load_settings():
+        pass
---
+++ classTask.py
@@ -0,0 +1,19 @@
+
+
+class Task:
+
+    def __init__(self):
+        self.content = ""
+        pass
+
+    def set_status(status_name):
+        pass
+
+    def get_status():
+        pass
+
+    def set_content(content):
+        pass
+
+    def get_content():
+        pass
