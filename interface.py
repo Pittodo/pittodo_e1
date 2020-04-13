ba85314345df84650829928caf0992297491391c
Hatoos <h4t00s@gmail.com>
2020-04-13 19:54:50 +0200

    BoxLayout added

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ kivy.require('1.11.1')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
 class PittodoMain(Widget):
