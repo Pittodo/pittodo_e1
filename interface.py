5517ba97e0c1889a99d9c3992aa1f7eaefa73422
Hatoos <h4t00s@gmail.com>
2020-04-14 18:41:50 +0200

    Connection to model repaired.

--- interface.py
+++ interface.py
@@ -7,26 +7,43 @@ from classModel import Model
 
 from debug import tic, toc
 
+# --- Widget inside window definition ---
 class PittodoMainWidget(QWidget):
     def __init__(self):
         super().__init__()
+        # Create object of Model in the widget
+        self.model = Model()
+        # Buttons creations
+        self.buttonAddTask = QPushButton("Add")
+        self.buttonDeleteTask = QPushButton("Delete")
+        self.buttonSave = QPushButton("Save")
+        self.buttonImport = QPushButton("Import")
+        self.buttonExport = QPushButton("Export")
 
-        self.buttonAddTask = QPushButton("AddTask")
+        # Labels
         self.text = QLabel("Pittodo")
-        # QWidget Layout
+        # QWidget Layouts
         self.left = QVBoxLayout()
+
+        # Buttons connect with layout
         self.left.addWidget(self.buttonAddTask)
+        self.left.addWidget(self.buttonDeleteTask)
+        self.left.addWidget(self.buttonSave)
+        self.left.addWidget(self.buttonImport)
+        self.left.addWidget(self.buttonExport)
+
         self.left.addWidget(self.text)
         self.setLayout(self.left)
 
          # Signals and Slots
-        self.buttonAddTask.clicked.connect(self.add_task)
+        self.buttonAddTask.clicked.connect(self.add_task_clicked)
 
     @Slot()
-    def add_task():
-        Model.add_task()
+    def add_task_clicked(self):
+        self.model.add_task()
 
 
+# --- Window class definition ---
 class MainWindow(QMainWindow):
     def __init__(self, widget):
         QMainWindow.__init__(self)
@@ -55,6 +72,6 @@ class MainWindow(QMainWindow):
 def run(self, model):
     self.model = model
 
-def add_task(self):
-    print("[PittodoApp] add_task()")
-    self.model.add_task()
+# def add_task(self):
+#     print("[PittodoApp] add_task()")
+#     self.model.add_task()
